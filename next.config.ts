fb5c07508b40217639a0c65642bff413426cc8d0
setsuwww <setsuprojectonly@gmail.com>
2025-08-22 08:17:19 +0700

    mau tarik

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,14 @@
-import withPWA from "next-pwa";
+import withPWAInit from "next-pwa";
 import type { NextConfig } from "next";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
 };
 
-export default withPWA({
-  ...nextConfig,
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-  },
-});
+export default withPWA(nextConfig);
